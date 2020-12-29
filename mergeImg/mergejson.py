63ac59dea44b2d4d838d45f884ba2a3340a11047
Sugar-Coder <13169670256@163.com>
2020-12-29 16:23:20 +0800

    implement the json rw

--- mergeImg/mergejson.py
+++ mergeImg/mergejson.py
@@ -3,11 +3,36 @@
 __author__ = 'Jinyang Shao'
 
 import json
+import glob
+import random
+
 import skimage.io as io
 import skimage.transform as transform
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-name = 'nada.jpg'
-name = name.split('.')[0]
-print(name)
+def loadImageInfo(fileprefix='nadaer'):
+    """
+    A generator, every time called return one object's image and its information
+    """
+    imgFiles = glob.glob(f'../results/images/{fileprefix}_*.jpg')
+    for i in range(len(imgFiles)):
+        objectImg = io.imread(f'../results/images/{fileprefix}_{i}.jpg')
+        f = open(f'../results/json/{fileprefix}_{i}.json', "r")
+        data = json.load(f)
+        f.close()
+        category = data['id']
+        mask = [np.array(w) for w in data['mask']]
+        mask = np.array(mask)
+        bbox = data['bbox']
+        score = float(data['score'])
+        yield objectImg, category, score, bbox, mask
+
+
+imgObjGenerator = loadImageInfo()
+objImg, category, score, bbox, mask = next(imgObjGenerator)
+print(category, bbox)
+# plt.imshow(objImg)
+# plt.imshow(mask)
+# plt.show()
